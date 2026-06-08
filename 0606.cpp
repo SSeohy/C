ae2f9252e14ce4e4573f63eee39d7540990695a6
SSeohy <seohh.jun@gmail.com>
2026-06-08 23:51:03 +0900

    C 코딩문제 업로딩 완료

--- 0606.cpp
+++ 0606.cpp
@@ -316,16 +316,127 @@
 
 
 //6번
+// int main(){
+//     int n1=1, n2=2, n3=3;
+//     int r1, r2, r3;
+//     r1 = (n2<=2) || (n3>3);
+//     r2 = !n3;
+//     r3 = (n1>1) && (n2<3);
+//     printf("%d", r3-r2+r1);
+//     return 0;
+// }
+
+
+//7번
+// int fn(char* a){
+//     int i=0;
+//     for(i=0; a[i] !='\0'; i++);
+//     return i;
+// }
+// int main(){
+//     char a[10] = "Hello";
+//     printf("%d", fn(a));
+//     return 0;
+// }
+
+
+
+//8번
+// struct st{ 
+//     int a; int c[10];
+// };
+// int main(){
+//     int i = 0;
+//     struct st ob1;
+//     struct st ob2;
+//     ob1.a = 0;
+//     ob2.a = 0;
+//     for(i=0; i<10; i++){
+
+//         ob1.c[i] = i;
+//         ob2.c[i] = ob1.c[i]+i;
+//     }
+//     for(i=0; i<10; i=i+2){
+//         ob1.a = ob1.a + ob1.c[i];
+//         ob2.a = ob2.a + ob2.c[i];
+//     }
+//     printf("%d", ob1.a + ob2.a);
+//     return 0;
+// }
+
+
+//9번
+// void Show(int a[][6]){
+//     int i, j;
+//     for(i=0; i<=4; i++){
+//         for(j=5; j>=0; j--){
+//             if(i==0 || i==4 || j==0 || j==5)
+//                 printf("%3d", a[i][j]);
+//             else
+//                 printf("   "); //공백3칸
+//         }
+//         printf("\n");
+//     }
+// }
+// int main(){
+//     int a[5][6] = {
+//         {24,25,26,27,28,29}, {18,19,20,21,22,23}, {12,13,14,15,16,17},
+//         {6,7,8,9,10,11}, {0,1,2,3,4,5}};
+//     Show(a);
+//     return 0;
+// }
+
+
+
+//10번(피보나치 수열)
+// int fn(int n){
+//     if(n==0)
+//         return 0;
+//     else if(n==1)
+//         return ???;
+//     else
+//         return ????;
+// }
+// int main(){
+//     printf("%d", fn(8));
+//     return 0;
+// }
+
+
+
+//11번
+// union Number{
+//     int i; float f; };
+// struct Data{
+//     union Number x;
+//     union Number y;
+//     char z;
+// };
+// void func(struct Data* a){
+//     if(a->z)
+//         a->x.i += a->y.f;
+//     else
+//         a->x.f += a->y.f;
+// }
+// int main(){
+//     struct Data a = {{.i = 5}, {.f = 3.5}, 1};
+//     func(&a);
+//     printf("%d\n", a.x.i);
+//     return 0;
+// }
+
+
+//12번
 int main(){
-    int n1=1, n2=2, n3=3;
-    int r1, r2, r3;
-    r1 = (n2<=2) || (n3>3);
-    r2 = !n3;
-    r3 = (n1>1) && (n2<3);
-    printf("%d", r3-r2+r1);
+    int x[4] = {2, 3}, i;
+    if(x[2] = x[0]>x[1] ? (x[1]>x[2] ? x[1]:x[2]) : (x[0]>x[2] ? x[0]:x[2]))
+        x[0] <<= 2;
+    else
+        x[1] <<= 2;
+    for(i=0; i<4; i++)
+        printf("%d", x[i]);
     return 0;
 }
-
 
 
 
